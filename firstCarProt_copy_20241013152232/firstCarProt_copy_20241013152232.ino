25cf51169b8860ef4766f5a32b14634e73afb696
mark-pog <M.pogrebinskyy@student.han.nl>
2024-10-13 17:59:42 +0200

    3 sensors

--- firstCarProt_copy_20241013152232/firstCarProt_copy_20241013152232.ino
+++ firstCarProt_copy_20241013152232/firstCarProt_copy_20241013152232.ino
@@ -1,12 +1,23 @@
+  
+ int frontTrigPin = 2;    // TRIG pin
+ int frontEchoPin = 1;    // ECHO pin
+ int rightTrigPin = 4;    // TRIG pin
+ int rightEchoPin = 3;    // ECHO pin
+ int leftTrigPin = 6;    // TRIG pin
+ int leftEchoPin = 5;    // ECHO pin
+ unsigned long previousMillis = 0;
+ int interval = 5000;
+ float distanceFront, distanceRight, distanceLeft;
 
-int trigPin = 7;    // TRIG pin
-int echoPin = 6;    // ECHO pin
-unsigned long previousSensorMillis = 0;
-unsigned long previousSensorInterval = 0;
-int interval = 5000;
-float duration_us, distance_cm;
-
-
+void printDistances(){
+  Serial.print("distances: ");
+  Serial.print(distanceFront);
+  Serial.print("---");
+  Serial.print(distanceRight);
+  Serial.print("---");
+  Serial.print(distanceLeft);
+  Serial.println(" cm");
+}
 
 void setup() {
   // pinMode(4, OUTPUT);
@@ -15,12 +26,17 @@ void setup() {
   // pinMode(7, OUTPUT);
   // pinMode(9, OUTPUT);
   // pinMode(11, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  pinMode(frontTrigPin, OUTPUT);
+  pinMode(frontEchoPin, INPUT);
+  pinMode(rightTrigPin, OUTPUT);
+  pinMode(rightEchoPin, INPUT);
+  pinMode(leftTrigPin, OUTPUT);
+  pinMode(leftEchoPin, INPUT);
 }
 
 
-int driveForward(int speed){
+void driveForward(){
     /*digitalWrite(6, HIGH);
     digitalWrite(7, LOW); 
     analogWrite(11, speed);
@@ -30,7 +46,7 @@ int driveForward(int speed){
     Serial.println("Driving Forward");
 }
 
-int driveRight(int speed){
+void driveRight(){
    /* digitalWrite(6, HIGH); 
     digitalWrite(7, LOW); 
     analogWrite(11, speed);
@@ -40,32 +56,59 @@ int driveRight(int speed){
     Serial.println("Driving Right");
 }
 
-void sensorsValues(){
+void sensorRightValue(){
   // generate 10-microsecond pulse to TRIG pin
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(rightTrigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(rightTrigPin, LOW);
 
   // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(echoPin, HIGH);
+  int duration_us = pulseIn(rightEchoPin, HIGH);
 
   // calculate the distance
-  distance_cm = 0.017 * duration_us;
+  distanceRight = 0.017 * duration_us;
 
   // print the value to Serial Monitor
-  Serial.print("distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
+
+}
+void sensorFrontValue(){
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(frontTrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(frontTrigPin, LOW);
+
+  // measure duration of pulse from ECHO pin
+  int duration_us = pulseIn(frontEchoPin, HIGH);
+
+  // calculate the distance
+  distanceFront = 0.017 * duration_us;
+
+}
+void sensorLeftValue(){
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(leftTrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(leftTrigPin, LOW);
+
+  // measure duration of pulse from ECHO pin
+  int duration_us = pulseIn(leftEchoPin, HIGH);
+
+  // calculate the distance
+  distanceLeft = 0.017 * duration_us;
 }
 
 
 void loop() {
-  sensorsValues();
-  if(distance_cm < 50){
-    driveRight(200);
-  } else {
-    driveForward(200);
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= 500){
+    printDistances();
+    sensorFrontValue();
+    if(distanceFront < 50){
+      driveRight();
+    } else {
+      driveForward();
+    }
+    previousMillis = currentMillis;
   }
-  delay(500);
 }
 
